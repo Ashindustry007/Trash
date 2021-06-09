9f03885e4189878085d607c0973cb06b84929159
Ashish Kumar Panda <44130583+Ashindustry007@users.noreply.github.com>
2021-06-09 19:21:25 +0530

    Update Chromedevtools.js

--- Chromedevtools.js
+++ Chromedevtools.js
@@ -2,3 +2,5 @@ function KeepClicking(){
    console.log("Clicking");
    document.querySelector("colab-toolbar-button#connect").click()
 }setInterval(KeepClicking,60000)
+
+#press F12 and run this code in Console so that colab session will not disconnect due to inactivity.
